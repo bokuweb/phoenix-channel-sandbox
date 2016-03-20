41b46fc859c1fd4e28d7a7285bcc40be44eb60eb
bokuweb <bokuweb12@gmail.com>
2016-03-20 17:08:16 +0900

    implement react and socket sample skeleton

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -2,4 +2,9 @@ defmodule PhoenixChannelSandbox.RoomChannel do
   use PhoenixChannelSandbox.Web, :channel
 
   def join("rooms:join", message, socket), do: {:ok, socket}
+
+  def handle_in("new:message", message, socket) do
+    broadcast! socket, "new:reply", %{msg: message["msg"]}
+    {:noreply, socket}
+  end
 end
--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,4 +1,14 @@
 import "../../../deps/phoenix_html/web/static/js/phoenix_html";
 import Socket from "./socket";
+import React from 'react';
+import { render } from 'react-dom';
 
+const socket = new Socket('/socket');
+socket.connect('rooms:join', {});
 
+setInterval(() => {
+  console.log('send');
+  socket.send();
+}, 1000);
+
+render(<h1>Hello, World!!</h1>, document.querySelector('#root'));
